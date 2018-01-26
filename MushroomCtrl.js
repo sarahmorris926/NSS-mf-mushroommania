3af5fc945ba404449680c6acc88e90bce9a92219
Sarah Morris <sarah.morris9@gmail.com>
2018-01-26 10:04:35 -0600

    Refactored to include factory but things not showing

---
+++ MushroomCtrl.js
@@ -0,0 +1,45 @@
+"use strict";
+
+angular
+  .module("MushroomStuff")
+  .controller("MushroomCtrl", function($scope, MushroomFactory) {
+    //
+    const printMushrooms = mushroomsToShow => {
+      $scope.mushroomList = mushroomsToShow;
+    };
+
+    // convert mushroom obj to array
+
+    const convertToArray = mushroomData => {
+      const mushroomArr = [];
+      for (let currentMushroom in mushroomData.data.mushrooms) {
+        mushroomArr.push(mushroomData.data.mushrooms[currentMushroom]);
+      }
+      return mushroomArr;
+    };
+
+    // print total mushrooms
+    MushroomFactory.getMushrooms()
+    .then(mushroomData => {
+      const mushroomArr = convertToArray(mushroomData);
+      printMushrooms(mushroomArr);
+    });
+
+    $scope.showPoisonous = () => {
+      MushroomFactory.getMushrooms()
+      .then(mushroomData => {
+        const mushroomArr = convertToArray(mushroomData);
+        const badMushrooms = mushroomArr.filter(mushroom => mushroom.edible !== true)
+        printMushrooms(badMushrooms);
+      });
+    };
+
+    $scope.showNonpoisonous = () => {
+      MushroomFactory.getMushrooms()
+      .then(mushroomData => {
+        const mushroomArr = convertToArray(mushroomData);
+        const goodMushrooms = mushroomArr.filter(mushroom => mushroom.edible === true)
+        printMushrooms(goodMushrooms);
+      });
+    };
+  });
---
+++ MushroomFactory.js
@@ -0,0 +1,18 @@
+"use strict";
+
+angular.module("MushroomStuff").factory("MushroomFactory", function($q, $http) {
+  let getMushrooms = () => {
+    return $q((resolve, reject) => {
+      $http
+        .get("https://mush-mania.firebaseio.com/mushrooms.json")
+        .then(mushrooms => {
+          resolve(mushrooms);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
+  };
+
+  return { getMushrooms };
+});
